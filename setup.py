5831d0c319ff6b9df930a90e30b9ea83ab9d0eac
Andy Kuszyk <andy.kuszyk@carfinance247.co.uk>
2017-11-07 13:24:16 +0000

    Updated to version 0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='jdss',
     packages=['jdss'],
-    version='0.2.1',
+    version='0.2.2',
     description='A command line tool for generating Jenkins summary reports for data science activities',
     author='Andy Kuszyk',
     author_email='pairofsocks@hotmail.co.uk',
